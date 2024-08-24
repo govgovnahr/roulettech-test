409cc52055cb5483f52fb43ee3483155fa01f419
govgovnahr <govindn3@illinois.edu>
2024-08-23 19:27:16 -0500

    including code to hook chatbot up to openAI

--- server/app/views.py
+++ server/app/views.py
@@ -6,9 +6,11 @@ from . models import *
 from . serializer import *
 from rest_framework.response import Response
 import json
+from openai import OpenAI
+import os
 # Create your views here.
 
-
+ai_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 class RecipeView(APIView):
     def get(self, request):
@@ -27,4 +29,19 @@ class RecipeView(APIView):
 def ai_assistant(request):
     #normally this would be with open_ai key but here we'll just have a baked response every time.
     print(request)
+    # if request.method == "POST":
+    #     data = json.loads(request.body)
+    #     inputMessage = data['message']
+    #     messages = [
+    #         {"role":"user", "content":inputMessage},
+    #     ]
+    #     chat_completion = ai_client.chat.completions.create(
+    #         messages=messages,
+    #         model="gpt-3.5-turbo"
+    #     )
+    #     bot_response = chat_completion.choices[0].message.content
+    #     return JsonResponse({"message": bot_response})
+    # else:
+    #     return JsonResponse({"error":"Invalid Request"}, status = 400)
+
     return JsonResponse({'message': "Hi! I'm a helpful AI assistant for help with your recipe!"})
